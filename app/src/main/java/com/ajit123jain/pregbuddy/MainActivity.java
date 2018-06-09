d96aba18e9a53f1fa3263e9fb46f8c36549d7bf6
ajit-999 <ajitkumarjain123@gmail.com>
2018-06-10 01:09:30 +0530

    FCM Added

--- app/src/main/java/com/ajit123jain/pregbuddy/MainActivity.java
+++ app/src/main/java/com/ajit123jain/pregbuddy/MainActivity.java
@@ -3,11 +3,15 @@ package com.ajit123jain.pregbuddy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pusher.pushnotifications.PushNotifications;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PushNotifications.start(getApplicationContext(), "YOUR_INSTANCE_ID");
+        PushNotifications.subscribe("hello");
     }
 }
